8e473052acb24b97fbf38f411bb92511fbdb6a6f
Alepacho <danildimaf@gmail.com>
2025-08-09 15:44:01 +0300

    disable debug

--- source/Config.hx
+++ source/Config.hx
@@ -1,6 +1,6 @@
 package;
 
 class Config {
-    public static inline final DEBUG:Bool = true; 
+	public static inline final DEBUG:Bool = false; 
     public static inline final GRAVITY:Float = 200;
 }
